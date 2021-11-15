dbe2cd2e8d472a3feb9ab5aa71f628d646209548
nivethanatrajan <nivethanatrajan26@gmail.com>
2021-11-15 17:11:05 +0530

    pussing 2folders

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
+import Users from "./users/pages/Users";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+
+function App() {
+  return (
+    <Router>
+      <MainNavigation />
+      <div></div>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/place/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
